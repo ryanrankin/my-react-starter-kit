71146c5370e530fcbcbbc623eaa51154cd922419
Ryan Rankin <Ryan.Rankin@EntouchControls.com>
2023-07-16 16:04:21 -0500

    Added bootstrap and created color theme

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
+import packageJson from '../package.json';
 import logo from './logo.svg';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import {Button, Container} from "react-bootstrap";
+import React, {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [appVersion] = useState(packageJson.version);
+    return (
+        <div className="App">
+            <Container>
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    <Button
+                        variant="primary"
+                        className="App-button"
+                        href="https://github.com/ryanrankin/my-react-starter-kit"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </Button>
+                </header>
+
+            </Container>
+            <footer className="App-footer">Version {appVersion}</footer>
+        </div>
+    );
 }
 
 export default App;
